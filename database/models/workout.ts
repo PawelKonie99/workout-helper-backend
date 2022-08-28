dc0519f4cc4facf59ab79c18c5fa9f272dcfd589
PawelKonie99 <pawel.konieczny21@gmail.com>
2022-08-28 14:47:48 +0200

    feat: add workout schema

--- database/models/workout.ts
+++ database/models/workout.ts
@@ -1,42 +1,54 @@
-import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
-import { ISet } from "../../interfaces/workoutInterface";
+import { Schema, model, Document } from "mongoose";
 
-export interface IWorkoutSchema extends mongoose.Document {
-  id: string;
-  setInfo: ISet[];
+export interface IWorkoutSeriesSchema {
+    workoutData: {
+        exerciseName: string;
+        repsQuantity: number;
+        seriesQuantity: number;
+        weightQuantity: number;
+    }[];
 }
 
-const workoutSchema = new mongoose.Schema({
-  workout: [
-    {
-      id: {
-        type: String,
-        required: true,
-      },
-      setInfo: [
+export interface IWorkoutSchema extends Document {
+    id: string;
+    workoutData: IWorkoutSeriesSchema;
+}
+
+const workoutSchema = new Schema<IWorkoutSchema>({
+    workoutData: [
         {
-          id: {
-            type: String,
-            required: true,
-          },
-          numberOfReps: {
-            type: Number,
-            required: true,
-          },
+            exerciseName: {
+                type: String,
+                required: true,
+            },
+            repsQuantity: {
+                type: String,
+                required: true,
+            },
+            seriesQuantity: {
+                type: String,
+                required: true,
+            },
+            weightQuantity: {
+                type: String,
+                required: true,
+            },
         },
-      ],
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
     },
-  ],
 });
 
 workoutSchema.plugin(uniqueValidator);
 workoutSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
-export const workoutModel = mongoose.model<IWorkoutSchema>("Workout", workoutSchema);
+export const workoutModel = model<IWorkoutSchema>("Workout", workoutSchema);
